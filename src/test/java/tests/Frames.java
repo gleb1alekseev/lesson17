5d1ff6a76240aefe62137a524bd2e2c3e31bf7ea
g.alekseev <gleb1alekseev@gmail.com>
2025-02-13 21:48:17 +0300

    lesson20

---
+++ src/test/java/tests/Frames.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Frames {
+    @Test
+    public void checkTextInFrame(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/iframe");
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        driver.switchTo().frame(iframe);
+        WebElement paragraph = driver.findElement(By.xpath("//body[@id='tinymce']/p"));
+        String text = paragraph.getText();
+        Assert.assertEquals(text, "Your content goes here.");
+        driver.switchTo().defaultContent();
+    }
+}
